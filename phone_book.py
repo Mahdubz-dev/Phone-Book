11e8d9b19df54b749e95bc895a886ef5bd51c248
A. Mahdi <amirmahdikarami1382@gmail.com>
2026-09-22 17:52:54 +0330

    Complete phone book project version 1

--- phone_book.py
+++ phone_book.py
@@ -8,7 +8,8 @@ menu_text = """
 │    3- Search for a phone number by name      │
 │    4- Remove contact                         │
 │    5- Amount of contact                      │
-│    6- Exit                                   │
+│    6- Show all contacts                      │
+│    7- Exit                                   │
 └──────────────────────────────────────────────┘
 
 """
@@ -35,8 +36,12 @@ def get_phone_number():
 """)
 
 
+def get_contact_name():
+    return input("• Enter the contact's name : ").strip()
+
+
 def add_contact():
-    contact_name = input("• Enter the contact's name : ")
+    contact_name = get_contact_name()
 
     if contact_name in contact_list:
 
@@ -52,6 +57,63 @@ def add_contact():
 ┌──────────────────────────────────┐
 │    Contact successfully added    │
 └──────────────────────────────────┘
+"""
+
+
+def add_number_to_contact():
+    contact_name = get_contact_name()
+
+    if contact_name not in contact_list:
+        return """
+┌───────────────────────────────────────┐
+│    No contact found with this name    │
+└───────────────────────────────────────┘
+"""
+    new_phone_number = get_phone_number()
+    contact_list[contact_name].append(new_phone_number)
+
+    return """
+┌─────────────────────────────────────────┐
+│    The new number successfully added    │
+└─────────────────────────────────────────┘
+"""
+
+
+def search_contact():
+    contact_name = get_contact_name()
+
+    if contact_name in contact_list:
+        phone_numbers = contact_list[contact_name]
+
+        phone_numbers_text = "\n".join(phone_numbers)
+        return f"""
+- {contact_name} :
+  {phone_numbers_text}
+"""
+
+    return """
+┌───────────────────────────────────────┐
+│    No contact found with this name    │
+└───────────────────────────────────────┘
+"""
+
+
+def remove_contact():
+    contact_name = get_contact_name()
+
+    if contact_name in contact_list:
+
+        del contact_list[contact_name]
+        return """
+┌────────────────────────────────────────┐
+│    The contact successfully removed    │
+└────────────────────────────────────────┘
+"""
+
+    return """
+┌───────────────────────────────────────┐
+│    No contact found with this name    │
+└───────────────────────────────────────┘
 """
 
 
@@ -90,14 +152,34 @@ def continue_cycle(result_message):
             continue_choice = input(continue_prompt).lower()
 
 
+def show_all_contacts():
+
+    if not contact_list:
+        return """
+┌─────────────────────────────┐
+│    Contact list is empty    │
+└─────────────────────────────┘
+"""
+
+    contact_text = ""
+
+    for contact_name, phone_numbers in contact_list.items():
+        contact_text += f"\n- {contact_name}\n"
+
+        for phone_number in phone_numbers:
+            contact_text += f"    {phone_number}\n"
+
+    return contact_text
+
+
 while True:
 
-    user_choice = input(f"{menu_text}• Enter a number between 1 to 6 : ")
+    user_choice = input(f"{menu_text}• Enter a number between 1 to 7 : ")
 
     result_message = ""
 
     # Exit
-    if user_choice == "6":
+    if user_choice == "7":
         print(exit_response)
         break
 
@@ -107,25 +189,29 @@ while True:
 
     # 2- Add number to contact
     elif user_choice == "2":
-        pass
+        result_message = add_number_to_contact()
 
     # 3- Search for a phone number by name
     elif user_choice == "3":
-        pass
+        result_message = search_contact()
 
     # 4- Remove contact
     elif user_choice == "4":
-        pass
+        result_message = remove_contact()
 
     # 5- Amount of contact
     elif user_choice == "5":
-        pass
+        result_message = f"Quantity of Contacts >>> {len(contact_list)}"
 
-    # Invalid choice
+    # 6- Show all contacts
+    elif user_choice == "6":
+        result_message = show_all_contacts()
+
+        # Invalid choice
     else:
         print("""
 ┌────────────────────────────┐
-│    Invalid Choice (1-6)    │
+│    Invalid Choice (1-7)    │
 └────────────────────────────┘
 """)
 
